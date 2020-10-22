ba2f2487d2661137ff6d77b9ad6686f2f91fceb6
Marc <mlanglois333@icloud.com>
2020-10-22 18:49:40 -0400

    asdf

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,11 +1,10 @@
 const FILES_TO_CACHE = [
-  '/',
-  '/index.html',
-  '/assets/css/styles.css',
-  '/assets/img/icon-192x192.png',
-  '/assets/img/icon-512x512.png',
-  '/assets/js/db.js',
-  '/assets/js/index.js',
+  './index.html',
+  './assets/css/styles.css',
+  './assets/img/icon-192x192.png',
+  './assets/img/icon-512x512.png',
+  './assets/js/db.js',
+  './assets/js/index.js',
   'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css',
   'https://cdn.jsdelivr.net/npm/chart.js@2.8.0"'
 ];
