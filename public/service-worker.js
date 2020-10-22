9946fa1841c51c31933928827a66796ff3199954
Marc <mlanglois333@icloud.com>
2020-10-22 17:02:17 -0400

    debug

--- public/service-worker.js
+++ public/service-worker.js
@@ -8,65 +8,55 @@ const FILES_TO_CACHE = [
   '/assets/js/index.js',
   'https://cdn.jsdelivr.net/npm/chart.js@2.8.0"'
 ];
-const CACHE_NAME = "static-cache-v2";
-const DATA_CACHE_NAME = "data-cache-v1";
+const PRECACHE = 'precache-v1';
+const RUNTIME = 'runtime';
 
-self.addEventListener("install", function (evt) {
-
-  evt.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches
+      .open(PRECACHE)
+      .then((cache) => cache.addAll(FILES_TO_CACHE))
+      .then(self.skipWaiting())
   );
-
-  self.skipWaiting();
+});
+self.addEventListener('activate', (event) => {
+  const currentCaches = [PRECACHE, RUNTIME];
+  event.waitUntil(
+    caches
+      .keys()
+      .then((cacheNames) => {
+        return cacheNames.filter((cacheName) => !currentCaches.includes(cacheName));
+      })
+      .then((cachesToDelete) => {
+        return Promise.all(
+          cachesToDelete.map((cacheToDelete) => {
+            return caches.delete(cacheToDelete);
+          })
+        );
+      })
+      .then(() => self.clients.claim())
+  );
 });
 
-
-self.addEventListener("activate", function (evt) {
-  evt.waitUntil(
-    caches.keys().then(keyList => {
-      return Promise.all(
-        keyList.map(key => {
-          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
-            console.log("Removing old cache data", key);
-            return caches.delete(key);
-          }
-        })
-      );
-    })
-  );
-
-  self.clients.claim();
-});
-
-
-self.addEventListener("fetch", function (evt) {
-  if (evt.request.url.includes("/api/")) {
-    evt.respondWith(
-      caches.open(DATA_CACHE_NAME).then(cache => {
-        return fetch(evt.request)
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== "GET") {
+    event.respondWith(fetch(event.request));
+    return;
+  } else if (event.request.url) {
+    event.respondWith(
+      caches.open(RUNTIME).then(cache => {
+        return fetch(event.request)
           .then(response => {
-
             if (response.status === 200) {
-              cache.put(evt.request.url, response.clone());
+              cache.put(event.request.url, response.clone());
             }
-
             return response;
           })
           .catch(err => {
-
-            return cache.match(evt.request);
+            return cache.match(event.request);
           });
       }).catch(err => console.log(err))
     );
-
     return;
   }
-
-  evt.respondWith(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.match(evt.request).then(response => {
-        return response || fetch(evt.request);
-      });
-    })
-  );
 });
